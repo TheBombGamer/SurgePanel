50e863ecbfe0cba8f4ff88e710641f8974b1edad
TheBombGamer <electr_stream@outlook.com>
2025-03-03 23:07:42 -0500

    fixed css issue

--- web/index.php
+++ web/index.php
@@ -6,13 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-
 </head>
-
 <body>
     <div class="header">
         <img class="logo" src='https://avatars.githubusercontent.com/u/201522923' alt='Surge Panel Logo'>
-        <a class="login" href="./css/login.php">Login</a>
+        <a class="login" href="./login.php">Login</a>
     </div>
     <div class="container">
         <div class="box">This is supposed to be your custom content. Put whatever you want here; just edit the ./index.php or ./index.html file.</div>
