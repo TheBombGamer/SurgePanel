b28fde6ce7f1595bfeca7bc685fd366717949450
TheBombGamer <electr_stream@outlook.com>
2025-03-03 23:02:28 -0500

    trertuciary

--- web/index.php
+++ web/index.php
@@ -2,10 +2,11 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="./css/styles.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
+
 </head>
 
 <body>
