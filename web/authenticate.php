e3c29593e506cbd69e753111b0f99cf75f3b3051
Kyle Speight <Electr_stream@outlook.com>
2025-03-03 21:15:28 -0800

    fixed php code bug

--- web/authenticate.php
+++ web/authenticate.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['loggedin'] = true;
             $_SESSION['username'] = $username;
             $_SESSION['id'] = $user['id'];
-            header('Location: panel.php');
+            header('Location: /panel/panel.php');
             exit;
         } else {
             echo "Invalid username or password.";
